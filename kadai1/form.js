9ee036deaa39e8d2771cd1fed09444a5244c298d
JuneNakajima <nakajimajn@sohobb.jp>
2022-04-06 18:04:08 +0900

    step3まで実装？

--- kadai1/form.js
+++ kadai1/form.js
@@ -1,4 +1,9 @@
+const taskList = [];
+
 const onClickButton = () => {
-    const yourName = document.getElementById("form").value;
-    console.log(yourName);
-}
+    const name = document.getElementById("form").value;
+    const task = document.getElementById("task").value;
+
+    taskList.push({name: name.value, task: task.value});
+    taskList.forEach((t, idx) => console.log(`${idx}: ${t.name}, ${t.task}`));
+};
