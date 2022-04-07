0fa044c5ed6817ff239b27ac26d0c8f6c634863a
JuneNakajima <nakajimajn@sohobb.jp>
2022-04-07 11:21:25 +0900

    コメント追加だけ

--- kadai1/form.js
+++ kadai1/form.js
@@ -3,6 +3,8 @@ const tasksList = [];
 const onClickButton = () => {
     const name = document.querySelector("#name");
     const task = document.querySelector("#task");
+
+    /* getElementByIDのほうが早いらしい。＃はいるらしい*/
   
     tasksList.push({ name: name.value, task: task.value });
     tasksList.forEach((t, idx) => console.log(`${idx}: ${t.name}, ${t.task}`));
